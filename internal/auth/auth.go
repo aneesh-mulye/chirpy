8f736dac56359b33d77f049b8a0d1161f40f5660
Aneesh Mulye <851233+aneesh-mulye@users.noreply.github.com>
2025-06-24 17:29:11 +0530

    Auth done, awaiting testing

--- internal/auth/auth.go
+++ internal/auth/auth.go
@@ -1,12 +1,17 @@
 package auth
 
 import (
+	"errors"
 	"fmt"
+	"time"
 
+	"github.com/golang-jwt/jwt/v5"
+	"github.com/google/uuid"
 	"golang.org/x/crypto/bcrypt"
 )
 
 const cost = 10
+const issuer = "chirpy"
 
 func HashPassword(password string) (string, error) {
 	hash, err := bcrypt.GenerateFromPassword([]byte(password), cost)
@@ -22,4 +27,47 @@ func CheckPasswordHash(hash, password string) error {
 		return fmt.Errorf("passwords and hash do not match: %w", err)
 	}
 	return nil
+}
+
+func MakeJWT(userID uuid.UUID, tokenSecret string,
+	expiresIn time.Duration) (string, error) {
+
+	timeNow := time.Now()
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.RegisteredClaims{
+		Issuer:    issuer,
+		IssuedAt:  jwt.NewNumericDate(timeNow),
+		ExpiresAt: jwt.NewNumericDate(timeNow.Add(expiresIn)),
+		Subject:   userID.String(),
+	})
+	ss, err := token.SignedString([]byte(tokenSecret))
+	if err != nil {
+		return "", fmt.Errorf("error creating JWT token: %w", err)
+	}
+	return ss, nil
+}
+
+func ValidateJWT(tokenString, tokenSecret string) (uuid.UUID, error) {
+	token, err := jwt.ParseWithClaims(tokenString, &jwt.RegisteredClaims{},
+		func(token *jwt.Token) (any, error) {
+			return []byte(tokenSecret), nil
+		})
+	if err != nil {
+		return uuid.UUID{}, fmt.Errorf("invalid JWT: %w", err)
+	}
+	if token.Method != jwt.SigningMethodHS256 {
+		return uuid.UUID{}, fmt.Errorf("unexpected signing method: %v",
+			token.Header["alg"])
+	}
+	claims, ok := token.Claims.(*jwt.RegisteredClaims)
+	if !ok {
+		return uuid.UUID{}, errors.New("invalid 'claims' in JWT")
+	}
+	if claims.Issuer != issuer {
+		return uuid.UUID{}, fmt.Errorf("invalid issuer: %v", claims.Issuer)
+	}
+	subjectUUID, err := uuid.Parse(claims.Subject)
+	if err != nil {
+		return uuid.UUID{}, fmt.Errorf("cannot parse UUID: %w", err)
+	}
+	return subjectUUID, nil
 }
